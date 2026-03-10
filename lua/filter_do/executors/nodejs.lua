6d6644f22da48e61c423ee100799a81a0c57d733
chaney <chaneyzorn@gmail.com>
2026-03-10 22:31:02 +0800

    fix(executor): fix node interpreter detection logic

--- lua/filter_do/executors/nodejs.lua
+++ lua/filter_do/executors/nodejs.lua
@@ -1,9 +1,9 @@
 ---@type filter_do.ExecutorInfo
 return {
   filter_cmd = function(ctx)
-    local node = vim.g.node_host_prog or vim.fn.exepath("node")
+    local node = vim.fn.exepath("node")
     if node == "" or node == nil then
-      local err_msg = "filter_do.nvim: node interpreter not found, please set g:node_host_prog"
+      local err_msg = "filter_do.nvim: node interpreter not found"
       vim.notify(err_msg, vim.log.levels.ERROR)
       return nil
     end
